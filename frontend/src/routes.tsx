830a4987ebb205744c432758e926ad8da8f03687
menezesbr <menezesbr@gmail.com>
2020-10-14 20:38:09 -0300

    Link entre paginas

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanageMap from './pages/OrphanageMaps';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
     return (
@@ -10,6 +12,9 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanageMap} />
+                
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
             </Switch>
         </BrowserRouter>
     );
