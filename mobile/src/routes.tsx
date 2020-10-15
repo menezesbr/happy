88c0fd40a14efa7475c63aabfa2ef05d13a92ef3
menezesbr <menezesbr@gmail.com>
2020-10-15 20:18:00 -0300

    [MOBILE] :: Criando páginas e rotas

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OrphanageMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="OrphanagesMap" component={OrphanageMap} />
+                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+            </Navigator>
+        </NavigationContainer>
+    );
+}
