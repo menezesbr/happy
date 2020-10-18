632eb796284af64624a7c12d055f205ce93df3c9
menezesbr <menezesbr@gmail.com>
2020-10-18 17:54:52 -0300

    Ajustes em detalhes do orfanato

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Image, View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
+import { Image, View, ScrollView, Text, StyleSheet, Dimensions, Linking } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Feather, FontAwesome } from '@expo/vector-icons';
 import { useRoute } from '@react-navigation/native';
 
 import mapMarkerImg from '../images/map-marker.png';
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton, TouchableOpacity } from 'react-native-gesture-handler';
 import api from '../services/api';
 
 interface OrphanageDetailsRouteParams {
@@ -47,6 +47,11 @@ export default function OrphanageDetails() {
                 <Text style={styles.description}>Carregando...</Text>
             </View>
         )
+    }
+
+    function handleOpenGoogleMapRoutes() {
+        Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${orphanage?.latitude},${orphanage?.longitude}`);
+        // o interrogação verifica se a propriedade existe antes de usar
     }
 
     return (
@@ -92,31 +97,41 @@ export default function OrphanageDetails() {
                 />
             </MapView>
 
-            <View style={styles.routesContainer}>
+            <TouchableOpacity onPress={handleOpenGoogleMapRoutes} style={styles.routesContainer}>
+            
                 <Text style={styles.routesText}>Ver rotas no Google Maps</Text>
-            </View>
+            </TouchableOpacity>
             </View>
         
             <View style={styles.separator} />
 
-            <Text style={styles.title}>Instruções para visita</Text>
-            <Text style={styles.description}>{orphanage.instructions}</Text>
+                <Text style={styles.title}>Instruções para visita</Text>
+                <Text style={styles.description}>{orphanage.instructions}</Text>
 
-            <View style={styles.scheduleContainer}>
-            <View style={[styles.scheduleItem, styles.scheduleItemBlue]}>
-                <Feather name="clock" size={40} color="#2AB5D1" />
-                <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>Segunda à Sexta {orphanage.opening_hours}</Text>
-            </View>
-            <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
-                <Feather name="info" size={40} color="#39CC83" />
-                <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>Atendemos fim de semana</Text>
-            </View>
+                <View style={styles.scheduleContainer}>
+                <View style={[styles.scheduleItem, styles.scheduleItemBlue]}>
+                    <Feather name="clock" size={40} color="#2AB5D1" />
+                    <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>Segunda à Sexta {orphanage.opening_hours}</Text>
+                </View>
+
+                {orphanage.open_on_weekends ? (
+                    <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
+                        <Feather name="info" size={40} color="#39CC83" />
+                        <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>Atendemos fim de semana</Text>
+                    </View>
+                ) : (
+                    <View style={[styles.scheduleItem, styles.scheduleItemRed]}>
+                        <Feather name="info" size={40} color="#FFBCD4" />
+                        <Text style={[styles.scheduleText, styles.scheduleTextRed]}>Não atendemos fim de semana</Text>
+                    </View>
+                )}
+                
             </View>
 
-            <RectButton style={styles.contactButton} onPress={() => {}}>
+            {/* <RectButton style={styles.contactButton} onPress={() => {}}>
                 <FontAwesome name="whatsapp" size={24} color="#FFF" />
                 <Text style={styles.contactButtonText}>Entrar em contato</Text>
-            </RectButton>
+            </RectButton> */}
         </View>
         </ScrollView>
     )
@@ -211,6 +226,13 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
 
+  scheduleItemRed: {
+    backgroundColor: '#FEF6F9',
+    borderWidth: 1,
+    borderColor: '#FFBCD4',
+    borderRadius: 20,
+  },
+
   scheduleText: {
     fontFamily: 'Nunito_600SemiBold',
     fontSize: 16,
@@ -224,6 +246,10 @@ const styles = StyleSheet.create({
 
   scheduleTextGreen: {
     color: '#37C77F'
+  },
+
+  scheduleTextRed: {
+    color: '#FF669D'
   },
 
   contactButton: {
