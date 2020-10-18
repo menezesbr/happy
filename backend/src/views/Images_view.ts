45cb20801b9f9efd3623775ca4eb3b341468c519
menezesbr <menezesbr@gmail.com>
2020-10-18 17:41:23 -0300

    mudança endereço para mobile

--- backend/src/views/Images_view.ts
+++ backend/src/views/Images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            url: `http://localhost:3333/uploads/${image.path}`,
+            url: `http://10.10.10.7:3333/uploads/${image.path}`,
         }
     },
 
